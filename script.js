9cf76733d116b1f71690ff26a7cf611c155191c2
Arjuniest <thedivinetreee@gmail.com>
2022-04-24 11:21:01 +0530

    Update JavaScript to detect key

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Keep typing");
+
+
+const textBox = document.querySelector("#textBox");
+const output = document.querySelector("#output");
+
+
+textBox.addEventListener('keydown' , event => 
+output.textContent = `You pressed "${event.key}".`);
+
