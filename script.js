7b8c60d82d3ca6bde4697e31d585b903879500b1
Arjuniest <thedivinetreee@gmail.com>
2022-04-24 11:32:01 +0530

    Add comments about arrow functions

--- script.js
+++ script.js
@@ -8,3 +8,8 @@ const output = document.querySelector("#output");
 textBox.addEventListener('keydown' , event => 
 output.textContent = `You pressed "${event.key}".`);
 
+
+//It was my first time learning about arrow functions so wanted to see it in action. 
+
+/* Instead of function(event), you write (event) => 
+*/
